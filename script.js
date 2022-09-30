caebcc2d2d47a6c0511397bf0254d4b36a8d4f4d
Maki0803 <112654905+Maki0803@users.noreply.github.com>
2022-09-30 16:27:14 -0600

    Add files via upload
    The program is great. Thank you for your feedback.

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function getSliderValue() {
+    var slider = document.getElementById("rate");
+    document.getElementById("sliderValue").innerHTML = slider.value
+}
+
+function compute(){
+    p = parseInt(document.getElementById("principal").value);
+    r = parseFloat(document.getElementById("rate").value);
+    y = parseInt(document.getElementById("years").value);
+
+    var totalInterestPaid = (p*r*y)/100;
+      
+    document.getElementById("totalAmount").innerHTML = p
+    document.getElementById("computeRate").innerHTML = r
+    document.getElementById("profit").innerHTML = totalInterestPaid
+    document.getElementById("year").innerHTML = 2022 + y
     
 }
         
